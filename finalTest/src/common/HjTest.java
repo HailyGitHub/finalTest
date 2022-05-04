66cd89f2cd2958ba22113144db87c39558e655c2
LAPTOP-U0EA9SLM\Haily <jjjjjung@gmail.com>
2022-05-04 13:53:47 +0900

    초기화

--- finalTest/src/common/HjTest.java
+++ finalTest/src/common/HjTest.java
@@ -3,16 +3,7 @@ package common;
 public class HjTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("서현정 1번!");
-<<<<<<< .merge_file_a04016
-		System.out.println("master 브랜치 3");
 		
-		
-=======
-		System.out.println("master 브랜치 2");
-		System.out.println("develope 1");
->>>>>>> .merge_file_a07224
 	}
 
 }
