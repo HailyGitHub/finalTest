a7b7d8d32aba45f9a214d98d0a6bac91d4a2d3ac
LAPTOP-U0EA9SLM\Haily <jjjjjung@gmail.com>
2022-05-04 13:35:28 +0900

    master master 브랜치 2

--- finalTest/src/common/HjTest.java
+++ finalTest/src/common/HjTest.java
@@ -5,6 +5,7 @@ public class HjTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("서현정 1번!");
+		System.out.println("master 브랜치 2");
 	}
 
 }
